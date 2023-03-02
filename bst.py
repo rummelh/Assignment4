02bdee3aaea3f3f32f107f72c88239608b8266fa
hrummel1 <123182419+hrummel1@users.noreply.github.com>
2023-03-01 20:06:02 -0600

    Working: add, contains, find_min, find_max, is_empty, make_empty, remove needs work:

--- bst.py
+++ bst.py
@@ -206,8 +206,10 @@ class BST:
         inorder_successor.left = remove_node.left
         if inorder_successor_right is not None:
             inorder_successor_parent.left = inorder_successor_right
-        else:
-            remove_node.right = None
+        inorder_successor.left = remove_node.left
+        inorder_successor.right = remove_node.right
+        #else:
+            #remove_node.right = None
             #remove_node.right = inorder_successor_right
 
 
